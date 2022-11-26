8649ab84cc608d7a068faf6151990dd7bdd46880
Alexander <fsa1netik@gmail.com>
2022-11-27 00:54:38 +0300

    HomeTaskOne
    HomeTaskOne(HWforKND11)

---
+++ HomeTaskOne/src/GoDutch.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class GoDutch {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        float friends, score, result;
+        float a = (float) 0.10;
+        System.out.println("Введіть кількість друзів:");
+        friends = s.nextFloat();
+        {
+            if (friends > 0) {
+                System.out.println("Введіть рахунок:");
+                score = s.nextFloat();
+                if (score > 0){
+                    result = (float) ((score + score * 0.10) / friends);
+                    System.out.println("З кожного по " + result);
+                }else{
+                    System.out.println("Bill total amount cannot be negative");
+                }
+            }else{
+                System.out.println("Number of friends cannot be negative or zero");
+            }
+        }
+    }
+}
